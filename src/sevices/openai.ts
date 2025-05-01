2139a4169b43878880a7bd2623bab1b12ee96e5f
Mariana <mariana.c.novaes22@gmail.com>
2025-05-01 01:52:30 -0300

    Corrigindo endpoint e configurando deploy

--- src/sevices/openai.ts
+++ src/sevices/openai.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://furia-backend-qb4q.onrender.com/chat";
 
 export const sendMessageToBot = async (mensagem: string): Promise<string> => {
   try {
